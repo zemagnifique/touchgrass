a62c5f662b7d560c92d08e6b9352df32510c31e9
Hassene Benammou <hassene.benammou@gmail.com>
2025-03-18 15:45:37 -0400

    docs

--- vite.config.js
+++ vite.config.js
@@ -10,15 +10,19 @@ export default defineConfig({
                 main: './index.html'
             },
             output: {
+                entryFileNames: 'assets/[name].js',
+                chunkFileNames: 'assets/[name]-[hash].js',
                 assetFileNames: (assetInfo) => {
                     const info = assetInfo.name.split('.');
                     const ext = info.pop();
                     const name = info.join('.');
-                    if (ext === 'css') return `assets/${name}.css`;
+                    
+                    if (name === 'main' || name === 'style') {
+                        return `assets/${name}.${ext}`;
+                    }
+                    
                     return `assets/[name]-[hash][extname]`;
-                },
-                chunkFileNames: 'assets/[name]-[hash].js',
-                entryFileNames: 'assets/[name]-[hash].js',
+                }
             }
         }
     }
